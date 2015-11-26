ab727cd1750cb885d77c1bc4a8c7f670f98f439b
Wilmer Arteaga <killerwilmer@gmail.com>
2015-11-25 23:45:47 -0500

    Step 4 - Adding/removing objects and Angular event handling

--- app.ng.js
+++ app.ng.js
@@ -3,5 +3,14 @@ if (Meteor.isClient) {
  
   angular.module('socially').controller('PartiesListCtrl', function ($scope, $meteor) {
     $scope.parties = $meteor.collection(Parties);
+
+    $scope.remove = function(party) {
+      // $scope.parties.splice($scope.parties.indexOf(party), 1);
+      $scope.parties.remove(party); //performance
+    };
+
+    $scope.removeAll = function(){
+      $scope.parties.remove();
+    };
   });
 }
